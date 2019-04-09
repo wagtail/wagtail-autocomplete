c03406c0414e8e3567c2c39f2d5ab9549ac6001f
Seb <seb@takeflight.com.au>
2019-04-09 15:01:14 +1000

    Working < 2.0

--- wagtailautocomplete/edit_handlers.py
+++ wagtailautocomplete/edit_handlers.py
@@ -19,29 +19,23 @@ if VERSION < (2, 0):
     # Wagtail 1.x
     from wagtail.wagtailadmin.edit_handlers import BaseFieldPanel
 
-    class AutocompletePanel(BaseFieldPanel):
+    class AutocompletePanel:
         def __init__(self, field_name, **kwargs):
             self.field_name = field_name
-            print(self.bound_field)
-
-        @property
-        def is_single(self):
-            # Should cover all manny-to-many relationships
-            return not issubclass(self.bound_field, ManyToManyField)
-
-        @property
-        def target_model(self):
-            return self.bound_field.remote_field.model
 
         def bind_to_model(self, model):
-            can_create = _can_create(self.target_model)
+            target_model = model._meta.get_field(self.field_name).remote_field.model
+            is_single = not issubclass(model._meta.get_field(self.field_name).__class__,
+                                       ManyToManyField)
+            can_create = _can_create(target_model)
+
             base = dict(
                 model=model,
                 field_name=self.field_name,
                 widget=type(
                     '_Autocomplete',
                     (Autocomplete,),
-                    dict(target_model=self.target_model, can_create=can_create, is_single=self.is_single),
+                    dict(target_model=target_model, can_create=can_create, is_single=is_single),
                 ),
             )
             return type('_AutocompleteFieldPanel', (BaseFieldPanel,), base)
@@ -50,7 +44,7 @@ else:
     from wagtail.admin.edit_handlers import FieldPanel
 
     class AutocompletePanel(FieldPanel):
-        def __init__(self, field_name, *args, **kwargs):
+        def __init__(self, field_name, **kwargs):
             super().__init__(field_name)
 
         @property
