f8b1e1c6210169a9eb44437a4980eb633a7b12cc
Tim Kamanin <tim@timonweb.com>
2020-02-10 11:26:12 +0100

    Replaces 'from django.contrib.staticfiles.templatetags.staticfiles import static' with 'from django.templatetags.static import static'
    As `static` can be imported from `django.templatetags.static` since the 1.10 Django version - see the [release note](https://docs.djangoproject.com/en/3.0/releases/1.10/#django-contrib-staticfiles) - and wagtail-autocomplete requires at least Django 1.11, the import from `django.contrib.staticfiles.templatetags.staticfiles` can be removed.

--- wagtailautocomplete/wagtail_hooks.py
+++ wagtailautocomplete/wagtail_hooks.py
@@ -1,7 +1,4 @@
-try:
-    from django.contrib.staticfiles.templatetags.staticfiles import static
-except ModuleNotFoundError:
-    from django.templatetags.static import static
+from django.templatetags.static import static
 
 from django.utils.html import format_html
 
