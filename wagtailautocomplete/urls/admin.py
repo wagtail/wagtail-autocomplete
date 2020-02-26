af4bd68fe40f0512cdc6227c9893ec2854c94c1c
Joep110 <61508200+Joep110@users.noreply.github.com>
2020-02-26 15:44:38 +0100

    FIX: Wagtail function moved require_admin_access
    Wagtail 2.8 require_admin_access has been moved from wagtail.admin.decorators to wagtail.admin.auth.
    Additionally we could create a check on what version Wagtail is being used to import the right import accordingly.

--- wagtailautocomplete/urls/admin.py
+++ wagtailautocomplete/urls/admin.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from wagtail.admin.decorators import require_admin_access
+from wagtail.admin.auth import require_admin_access
 
 from wagtailautocomplete.views import create, objects, search
 
