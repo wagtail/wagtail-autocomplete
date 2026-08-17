99a1ebca899043e2fe335e7cb49fa6165bae1cf2
Will Barton <will@brtns.com>
2026-08-17 08:06:11 -0400

    Add tox -e interactive to run interactively
    This adds a Wagtail conventional `interactive` env to tox, letting you run and open the app in a web browser, browse to snippets, and test out the functionality.
    
    To do this it adds minimal boilerplate to the `wagtailautocomplete.tests.testapp` to add the test models as snippets so they're exposed in the admin, and adds the requisite settings to make the testapp runnable as a Wagtail site.

--- wagtailautocomplete/tests/settings.py
+++ wagtailautocomplete/tests/settings.py
@@ -1,47 +1,61 @@
-SECRET_KEY = 'NOTSECRET'
+SECRET_KEY = "NOTSECRET"
+
+DEBUG = True
+
+ALLOWED_HOSTS = ["*"]
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'wagtailautocomplete',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "wagtailautocomplete.sqlite",
     }
 }
 
 INSTALLED_APPS = (
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.staticfiles',
-    'wagtail',
-    'wagtail.documents',
-    'wagtail.images',
-    'wagtail.admin',
-    'wagtail.sites',
-    'wagtail.users',
-    'taggit',
-    'wagtailautocomplete',
-    'wagtailautocomplete.tests.testapp',
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.messages",
+    "django.contrib.sessions",
+    "django.contrib.staticfiles",
+    "wagtail",
+    "wagtail.documents",
+    "wagtail.images",
+    "wagtail.admin",
+    "wagtail.sites",
+    "wagtail.users",
+    "taggit",
+    "wagtailautocomplete",
+    "wagtailautocomplete.tests.testapp",
+    "wagtail.snippets",
 )
 
 MIDDLEWARE = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 )
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.request',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.request",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-ROOT_URLCONF = 'wagtailautocomplete.tests.testapp.urls'
+ROOT_URLCONF = "wagtailautocomplete.tests.testapp.urls"
 
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+STATIC_URL = "/static/"
+MEDIA_URL = "/media/"
+
+WAGTAIL_SITE_NAME = "wagtail-autocomplete test site"
+
+WAGTAILADMIN_BASE_URL = "http://localhost:8020"
